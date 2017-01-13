90c61d2505464ec839da3558e59bafa875ef38fe
Diego González Morín <Diego Gonzlez Morn>
2017-01-13 20:46:54 +0100

    m

--- Matlab Files/Main.m
+++ Matlab Files/Main.m
@@ -1,25 +1,40 @@
 %% Section - Only Particle Filter
 
+%Clear everything
 clear all;
 close all;
+
+PARTICLES = 1;
+KALMAN = 2;
+BOTH = 3;
+filt_mod = KALMAN;
+
+%Path 
 addpath(genpath('Image_Transformation'));
 addpath(genpath('Kalman'));
 addpath(genpath('Particles'));
 
-%set to one if some areas want to be erased to check how god does the
-%kalman filter works
+%Set OBSTACLES to one if some areas want to be erased to the performance of
+%the filters when occlusions occur
 OBSTACLES = 1;
+
+%Set: verbose = 0 - No output
+%     verbose = 1 - Error plotting
+%     verbose = 2 - Real time video plotting
 verbose = 2;
 
 
 %Video input file
 v = VideoReader('Videos/NES_Longplay_[456_Pinball.mov');
 
-%Specify that reading should begin 2.5 seconds from the beginning of the video.
+%Specify when in the video (in s) should the program start reading
 v.CurrentTime = 255;
 
+%Set the finishing time. Length then will be ENDING - v.CurrentTime
 ENDING = 260;
 
+%Variable to ensure correct representation of particles and estimated
+%estates
 threshold_square = 30;
 
 %Parameter to decide the colour filtering
@@ -36,10 +51,29 @@ particle_size = 1;
 %Size of the centroid
 c_size = 4;
 
+%%%%%%%%%%%%%%%%%%%
+%%PArticle filter%%
+%%%%%%%%%%%%%%%%%%%
+
 %Get image size x - vertical y - horizontal
 [xp,yp,~] = size(readFrame(v));
 % Parameter Initialization
 [Sp,Rp] = init_Particles(xp,yp);
+
+
+%%%%%%%%%%%%%%%%%
+%%Kalman filter%%
+%%%%%%%%%%%%%%%%%
+
+count = 0;
+
+%Parameters initialization
+mmodel = 0; % motion model: 0 (constant speed) or 1 (constant acceleration)
+[R,Q,A,C] = kalmanInit(1, mmodel);
+mu = 0;
+Sigma = 0;
+error = [];
+centroid = [1 1];
 
 
 while (hasFrame(v) && v.currentTime <= ENDING)
@@ -59,32 +93,92 @@ while (hasFrame(v) && v.currentTime <= ENDING)
 
     [RGB, out] = imageTransformation(vidFrame,colour_thres,[187,187,187],c_thres);
     
-    %Apply the particle filter algorithm
-    [centroid, Sp,vidFrame] = Particle_filter(vidFrame, RGB, out, Sp, Rp , verbose);
+    if filt_mod == PARTICLES
+
+            %Apply the particle filter algorithm
+            [centroid, Sp,vidFrame] = Particle_filter(vidFrame, RGB, out, Sp, Rp , verbose);
+
+    elseif filt_mod == KALMAN
+
+            [B,L] = bwboundaries(out,'noholes');
+            %Check roundness
+            stats = regionprops(L,'Area','Centroid');
+            %Compute the number of white pixels in the image to calculate the
+            %oclusion
+            w_pix = sum(sum(out));
+            %Option B
+            if w_pix < 10
+                centroid = centroid;
+                distance = 0;
+                param = 3;
+            elseif w_pix < 70
+                param = 3;
+                centroid(1) = round(stats(1).Centroid(2));
+                centroid(2) = round(stats(1).Centroid(1));
+                distance = sqrt(round(stats(1).Area)/pi);
+            elseif w_pix < 100
+                param = 2;
+                centroid(1) = round(stats(1).Centroid(2));
+                centroid(2) = round(stats(1).Centroid(1));
+                distance = sqrt(round(stats(1).Area)/pi);
+            else
+                param = 1;
+                centroid(1) = round(stats(1).Centroid(2));
+                centroid(2) = round(stats(1).Centroid(1));
+                distance = sqrt(round(stats(1).Area)/pi);
+            end
+            %Parameters initialization
+            [R,Q,A,C] = kalmanInit(param, mmodel);
+            %Apply the Kalman filter algorithm
+            [count,mu,Sigma, vidFrame] = KalmanAlgorithm(vidFrame,count, mu, Sigma, centroid, ...
+                                                       A, R, C, Q, mmodel);
+
+    elseif filt_mod == BOTH
+    end
+    
 
     if verbose == 2
-        %This function represents the particles in the binary and original
-        %pictures and compute the distances of each particle to the centroid in
-        %ascending order
-        [vidFrame, RGB, distance] = particle_distance_and_out( vidFrame ,RGB, Sp,centroid,particle_size,c_size );
-
-        vidFrame(abs(round(centroid(1))-4 + 1):round(centroid(1))+4,abs(round(centroid(2))-4 +1):round(centroid(2))+4,3) = 255;
-        vidFrame(abs(round(centroid(1))-4 + 1):round(centroid(1))+4,abs(round(centroid(2))-4 +1):round(centroid(2))+4,1:2) = 0;
-        
-
-        [max_distance_x, max_distance_y] = rect_size(xp,yp,centroid(1),centroid(2),threshold_square,distance,Sp);
-
-        image(vidFrame); axis image;
+           if filt_mod == PARTICLES
+                    %This function represents the particles in the binary and original
+                    %pictures and compute the distances of each particle to the centroid in
+                    %ascending order
+                    [vidFrame, RGB, distance] = particle_distance_and_out( vidFrame ,RGB, Sp,centroid,particle_size,c_size );
+                    
+                    vidFrame(abs(round(centroid(1))-4 + 1):round(centroid(1))+4,abs(round(centroid(2))-4 +1):round(centroid(2))+4,3) = 255;
+                    vidFrame(abs(round(centroid(1))-4 + 1):round(centroid(1))+4,abs(round(centroid(2))-4 +1):round(centroid(2))+4,1:2) = 0;
+                    
+                    [max_distance_x, max_distance_y] = rect_size(xp,yp,centroid(1),centroid(2),threshold_square,distance,Sp);
+                    image(vidFrame); axis image;
     
-        %For other outputs
-    %     subplot(1,2,1); image(RGB); axis image
-    %     hold on
-    %     %rectangle('position',[abs(mu(2)-max_distance_y_K) abs(mu(1)-max_distance_x_K) abs(2*max_distance_y_K) abs(2*max_distance_x_K)], 'EdgeColor','g')
-    %     %hold off
-    %     subplot(1,2,2); image(vidFrame); axis image
-        hold on
-        rectangle('position',[abs(centroid(2)-max_distance_y) abs(centroid(1)-max_distance_x) abs(2*max_distance_y) abs(2*max_distance_x)], 'EdgeColor','g')
-        hold off
+                    %For other outputs
+                %     subplot(1,2,1); image(RGB); axis image
+                %     hold on
+                %     %rectangle('position',[abs(mu(2)-max_distance_y_K) abs(mu(1)-max_distance_x_K) abs(2*max_distance_y_K) abs(2*max_distance_x_K)], 'EdgeColor','g')
+                %     %hold off
+                %     subplot(1,2,2); image(vidFrame); axis image
+                    hold on
+                    rectangle('position',[abs(centroid(2)-max_distance_y) abs(centroid(1)-max_distance_x) abs(2*max_distance_y) abs(2*max_distance_x)], 'EdgeColor','g')
+                    hold off
+
+            elseif filt_mod == KALMAN
+                     %Print the estimated estate
+                     vidFrame(abs(round(mu(1))-4 + 1):round(mu(1))+4,abs(round(mu(2))-4 +1):round(mu(2))+4,3) = 255;
+                     vidFrame(abs(round(mu(1))-4 + 1):round(mu(1))+4,abs(round(mu(2))-4 +1):round(mu(2))+4,1:2) = 0;
+
+                     [max_distance_x_K, max_distance_y_K] = rect_size(xp,yp,mu(1),mu(2),threshold_square,distance);
+                   image(vidFrame); axis image;
+                %For other outputs
+                %     subplot(1,2,1); image(RGB); axis image
+                %     hold on
+                %     %rectangle('position',[abs(mu(2)-max_distance_y_K) abs(mu(1)-max_distance_x_K) abs(2*max_distance_y_K) abs(2*max_distance_x_K)], 'EdgeColor','g')
+                %     %hold off
+                %     subplot(1,2,2); image(vidFrame); axis image
+                    hold on
+                    rectangle('position',[abs(mu(2)-max_distance_y_K) abs(mu(1)-max_distance_x_K) abs(2*max_distance_y_K) abs(2*max_distance_x_K)], 'EdgeColor','g')
+                    hold off
+            
+            elseif filt_mod == BOTH
+            end
 
         pause(abs((1/v.FrameRate)-toc));
     elseif verbose == 1

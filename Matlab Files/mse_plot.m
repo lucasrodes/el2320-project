4fc2f4463dac27a1c2391057ba484fe7909bbc9b
diegomorin8 <diegomorin8@gmail.com>
2017-01-15 18:21:06 +0100

    Update mse_plot.m

--- Matlab Files/mse_plot.m
+++ Matlab Files/mse_plot.m
@@ -4,7 +4,7 @@ function error = mse_plot(estimated_st, out)
     stats = regionprops(L,'Area','Centroid');
     %Compute the number of white pixels in the image to calculate the
     %oclusion
-    w_pix = sum(sum(out))
+    w_pix = sum(sum(out));
     if w_pix >= 100
         centroid(1) = round(stats(1).Centroid(2));
         centroid(2) = round(stats(1).Centroid(1));
